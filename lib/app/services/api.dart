72c350de2f602e909330bbbb56f99877e0203acc
Gorka <codinglikeamonster@gmail.com>
2020-10-19 19:26:04 +0200

    Completar la clase api.dart

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -1,6 +1,8 @@
 import 'package:covid19_app/app/services/api_keys.dart';
 import 'package:flutter/foundation.dart';
 
+enum Endpoint { cases, casesSuspected, casesConfirmed, deaths, recovered }
+
 class API {
   API({@required this.apiKey});
   final String apiKey;
@@ -10,4 +12,15 @@ class API {
   static final String host = "ncov2019-admin.firebaseapp.com";
 
   Uri tokenUri() => Uri(scheme: 'https', host: host, path: 'token');
+
+  Uri endpointUri(Endpoint endpoint) =>
+      Uri(scheme: 'https', host: host, path: _paths[endpoint]);
+
+  static Map<Endpoint, String> _paths = {
+    Endpoint.cases: "cases",
+    Endpoint.casesConfirmed: "casesConfirmed",
+    Endpoint.casesSuspected: "casesSuspected",
+    Endpoint.deaths: "deaths",
+    Endpoint.recovered: "recovered"
+  };
 }
