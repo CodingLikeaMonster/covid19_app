0870a14053099b6f985786351749ded8e91e82c1
Gorka Aldasoro <gorkaa@anasinf.info>
2020-10-21 16:49:04 +0200

    Devolver todos los eventos de golpe cuando se refresca la pagina

--- lib/app/repositories/data_repository.dart
+++ lib/app/repositories/data_repository.dart
@@ -1,3 +1,4 @@
+import 'package:covid19_app/app/repositories/endpoints_data.dart';
 import 'package:covid19_app/app/services/api.dart';
 import 'package:covid19_app/app/services/api_service.dart';
 import 'package:flutter/foundation.dart';
@@ -25,5 +26,37 @@ class DataRepository {
       }
       rethrow;
     }
+  }
+
+  Future<EndpointsData> _getAllEndpointsData() async {
+    /*  final cases = await apiService.getEndPointData(
+        accessToken: _accessToken, endPoint: Endpoint.cases);
+    final casesConfirmed = await apiService.getEndPointData(
+        accessToken: _accessToken, endPoint: Endpoint.casesConfirmed);
+    final casesSuspected = await apiService.getEndPointData(
+        accessToken: _accessToken, endPoint: Endpoint.casesSuspected);
+    final deaths = await apiService.getEndPointData(
+        accessToken: _accessToken, endPoint: Endpoint.deaths);
+    final recovered = await apiService.getEndPointData(
+        accessToken: _accessToken, endPoint: Endpoint.recovered); */
+    final values = await Future.wait([
+      apiService.getEndPointData(
+          accessToken: _accessToken, endPoint: Endpoint.cases),
+      apiService.getEndPointData(
+          accessToken: _accessToken, endPoint: Endpoint.casesSuspected),
+      apiService.getEndPointData(
+          accessToken: _accessToken, endPoint: Endpoint.casesConfirmed),
+      apiService.getEndPointData(
+          accessToken: _accessToken, endPoint: Endpoint.deaths),
+      apiService.getEndPointData(
+          accessToken: _accessToken, endPoint: Endpoint.recovered)
+    ]);
+    return EndpointsData(values: {
+      Endpoint.cases: values[0],
+      Endpoint.casesSuspected: values[1],
+      Endpoint.casesConfirmed: values[2],
+      Endpoint.deaths: values[3],
+      Endpoint.recovered: values[4],
+    });
   }
 }
---
+++ lib/app/repositories/endpoints_data.dart
@@ -0,0 +1,15 @@
+import 'package:covid19_app/app/services/api.dart';
+import 'package:flutter/foundation.dart';
+
+class EndpointsData {
+  EndpointsData({@required this.values});
+  final Map<Endpoint, int> values;
+  int get cases => values[Endpoint.cases];
+  int get casesSuspected => values[Endpoint.casesSuspected];
+  int get casesConfirmed => values[Endpoint.casesConfirmed];
+  int get deaths => values[Endpoint.deaths];
+  int get recovered => values[Endpoint.recovered];
+  @override
+  String toString() =>
+      'Casos: $cases, Sospechosos: $casesSuspected, Confirmados: $casesConfirmed, Muertes: $deaths, Recuperados: $recovered';
+}
